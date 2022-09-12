ed42d0d4016d10bfb738d8e5dab2b4944d54e6c1
Retro <htoomyataung382@gmail.com>
2022-09-12 19:43:50 +0630

    my code is terrible send help

--- src/utils/Queue.js
+++ src/utils/Queue.js
@@ -1,20 +1,24 @@
 const voice = require('./voice.js')
-const play = require('play-dl')
-
-class QueueBuilder {
+const { getVoiceConnection } = require('@discordjs/voice')
+const EventEmiter = require('node:events')
+class QueueBuilder extends EventEmiter {
     constructor(guild, options) {
+        super()
         this.guild = guild
         this.options = options
     }
-    async create() {
+    async create(audioPlayer) {
         const queue = {
             guild: this.guild,
             tracks: [],
             isPlaying: false,
+            connection: getVoiceConnection(this.guild.id) || undefined,
             play: async (searchRes) => {
                 const searchedURL = searchRes?.url
+                if(!searchedURL) throw new Error('[ Dismusic Error ] Search result must be a type of searched track. Got ' + typeof searchRes + " instead")
                 const isThereCustomExtractor = this.options.useExtractor ? true : false
-
+                const isVoiceConnected = getVoiceConnection(this.guild.id) || null
+                if(!isVoiceConnected) throw new Error('[ Dismusic Error ] Player must be connected to voice before playing anything')
                 let stream, resource
                 
                 if(isThereCustomExtractor) {
@@ -29,12 +33,25 @@ class QueueBuilder {
                     }
                 } else {
                     try {
-                        stream = await play.stream(searchedURL)
+                        stream = (await voice.getStream(searchedURL)).stream
                     } catch (error) {
                         throw new Error('[ Dismusic Error ] There has been an error while building your stream. The Error is as follows\n' + error)
                     }
                 }
+                isVoiceConnected.subscribe(audioPlayer)
+                if(resource) audioPlayer.play(resource)
+                this.emit('EmitTrackStart', true, searchRes)
+                this.resource = resource
+            },
+            async connectTo(channel) {
+                const connection = await voice.joinTo(this.guild, channel)
+                if(!connection) throw new Error('[ Dismusic Error ] There was an error while trying to connect to a voice channel')
+                return connection
             }
+        }
+        return {
+            queue: queue,
+            resource: this.resource
         }
     }
 }
