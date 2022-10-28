e6952a346a545f876b408073e6260a7052281e03
Retro <htoomyataung382@gmail.com>
2022-10-28 20:55:44 +0630

    docs inital commit

--- RepeatMode.js
+++
@@ -1,8 +0,0 @@
-var QueueRepeatMode;
-(function (QueueRepeatMode) {
-    QueueRepeatMode["NONE"] = "none";
-    QueueRepeatMode["QUEUE"] = "queue";
-    QueueRepeatMode["SONG"] = "song";
-})(QueueRepeatMode || (QueueRepeatMode = {}));
-
-module.exports = QueueRepeatMode;
--- src/constants/regex.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    soundCloud: /(^(https:)\/\/(soundcloud.com)\/)/,
-    spotify: /(^(https:)\/\/(open.spotify.com)\/(track|playlist|album)\/)/,
-    youtube: /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/,
-    spotifyPlaylist: /(^(https:)\/\/(open.spotify.com)\/(playlist)\/)/
-}
