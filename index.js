7d12af9803fd34dbfa63571c0fc83fc5d812ef2e
Retro <htoomyataung382@gmail.com>
2023-03-07 20:53:33 +0630

    Initialize Next-Gen V3

--- index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    Player: require('./src/main'),
-    version: require('./package.json').version,
-    QueueRepeatMode: require("./RepeatMode.js"),
-    RadioClient: require('./src/radio.js')
-}
